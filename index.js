e7a0e0598a601daa22698dfa1ff1bbd312fb6708
ailmari <arttu.niemela@trad.fi>
2020-06-14 12:43:21 +0300

    Add express app

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Constantine XI -> 11')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Constantine XI -> 11')
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Contatine service running on port ${PORT}`)
+})
